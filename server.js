d7cb37068122513844dfbe625658d7e138503d8e
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-08 10:43:24 -0400

    separate root routes into own file

--- server.js
+++ server.js
@@ -25,20 +25,17 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
+const rootRoutes = require("./routes/rootRoutes");
 const usersRoutes = require("./routes/userRoutes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
+app.use("/", rootRoutes(db));
 app.use("/api/users", usersRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
-
-// Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
