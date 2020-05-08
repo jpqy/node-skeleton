b13d2fe82b782227c52a52f47b7ec78096a57fac
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-05-08 11:43:26 -0400

    modify users schema/seeds to be more useful

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users
+  (first_name, last_name, email, password)
+VALUES
+  ('Aaron', 'Aaronson', 'a@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'); --password is hash of 'password'
